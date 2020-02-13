342e3116c369ced4286f606fd3da3f1b9308005d
Michal Ajduk <michal.ajduk@canonical.com>
2020-02-13 14:42:29 +0100

    Extend OCIImageResource tests

--- test/oci_image_resource_test.py
+++ test/oci_image_resource_test.py
@@ -14,11 +14,78 @@ from ops.model import (
 
 sys.path.append('src')
 from resources import (
-    OCIImageResource
+    OCIImageResource,
+    MissingResourceError,
+    InvalidResourceError,
 )
+import pytest
 
 
 class OCIImageResourceTest(unittest.TestCase):
+
+    def test_fetch_image_info_failed(self):
+        # Setup
+        mock_resource_name = f"{uuid4()}"
+
+        mock_path_obj = MagicMock(Path)
+        mock_path_obj.exists.return_value = False
+
+        mock_resources_adapter = MagicMock(Resources)
+        mock_resources_adapter.fetch.return_value = mock_path_obj
+
+        # Exercise
+        image_resource = OCIImageResource(resource_name=mock_resource_name)
+
+        # Assertions
+        with pytest.raises(MissingResourceError):
+            assert image_resource.fetch(mock_resources_adapter)
+        assert mock_resources_adapter.fetch.call_count == 1
+        assert mock_resources_adapter.fetch.call_args == \
+            call(mock_resource_name)
+
+    def test_resource_yaml_error(self):
+        # Setup
+        mock_resource_name = f"{uuid4()}"
+
+        mock_path_obj = MagicMock(Path)
+        mock_path_obj.exists.return_value = True
+        mock_path_obj.read_text.return_value = f"""
+        \0GIBRISH
+        """
+
+        mock_resources_adapter = MagicMock(Resources)
+        mock_resources_adapter.fetch.return_value = mock_path_obj
+
+        # Exercise
+        image_resource = OCIImageResource(resource_name=mock_resource_name)
+
+        # Assertions
+        with pytest.raises(InvalidResourceError):
+            assert image_resource.fetch(mock_resources_adapter)
+        assert mock_resources_adapter.fetch.call_count == 1
+        assert mock_resources_adapter.fetch.call_args == \
+            call(mock_resource_name)
+
+    def test_resource_yaml_blank(self):
+        # Setup
+        mock_resource_name = f"{uuid4()}"
+
+        mock_path_obj = MagicMock(Path)
+        mock_path_obj.exists.return_value = True
+        mock_path_obj.read_text.return_value = None
+
+        mock_resources_adapter = MagicMock(Resources)
+        mock_resources_adapter.fetch.return_value = mock_path_obj
+
+        # Exercise
+        image_resource = OCIImageResource(resource_name=mock_resource_name)
+
+        # Assertions
+        with pytest.raises(MissingResourceError):
+            assert image_resource.fetch(mock_resources_adapter)
+        assert mock_resources_adapter.fetch.call_count == 1
+        assert mock_resources_adapter.fetch.call_args == \
+            call(mock_resource_name)
 
     def test_fetch_image_info_succesfully(self):
         # Setup
