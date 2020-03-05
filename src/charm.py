4334a1b3e27cdafd3d75f698091b3a5231f5ff46
Michal Ajduk <michal.ajduk@canonical.com>
2020-03-05 20:42:48 +0100

    Refactor with FrameworkWrapper
    Refactor with wrapper layer over Framework and separated handlers.

--- src/charm.py
+++ src/charm.py
@@ -6,147 +6,78 @@ sys.path.append('lib')
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import (
-    ActiveStatus,
-    BlockedStatus,
-    WaitingStatus,
-    MaintenanceStatus,
-)
-from resources import OCIImageResource
-import json
-import subprocess
 
+from resources import OCIImageResource
+from wrapper import FrameworkWrapper
+from k8s import K8sPod
+from observers import (
+    ConfigChangeObserver,
+    StatusObserver,
+    RelationObserver,
+)
+from builders import MongoBuilder
+import logging
+
+logger = logging.getLogger()
 
 class MongoDbCharm(CharmBase):
-    state = StoredState()
+    _state = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.start, self.on_start)
-        for event in (self.on.start,
-                      self.on.upgrade_charm,
-                      self.on.config_changed):
-            self.framework.observe(event, self.on_start)
-            self.framework.observe(self.on.update_status, self.on_update_status)
-            self.framework.observe(self.on.mongo_relation_joined, self.on_mongo_relation_joined)
-        self.mongodb_image = OCIImageResource('mongodb-image')
-        self.sidecar_image = OCIImageResource('mongodb-sidecar-image')
+        self._framework_wrapper = FrameworkWrapper(self.framework, self._state)
+        self._resources = {
+            'mongodb-image': OCIImageResource('mongodb-image')
+        }
 
-    def _make_container_spec(self):
-        return {
-                'name': self.framework.model.app.name,
-                'imageDetails': {
-                    'imagePath': self.mongodb_image.image_path,
-                    'username': self.mongodb_image.username,
-                    'password': self.mongodb_image.password,
-                },
-                'command': [
-                    'mongod',
-                    '--bind_ip',
-                    '0.0.0.0',
-                ],
-                'ports': [{
-                    'containerPort':
-                    self.framework.model.config['advertised-port'],
-                    'protocol': 'TCP',
-                }],
-                'config': {
-                    'ALLOW_ANONYMOUS_LOGIN': 'yes'
-                },
-                'readinessProbe': {
-                  'tcpSocket':{
-                    'port': self.framework.model.config['advertised-port'],
-                    },
-                  'timeoutSeconds': 5,
-                  'periodSeconds': 5,
-                  'initialDelaySeconds': 10,
-                },
-                'livenessProbe': {
-                  'exec': {
-                      'command':[
-                      '/bin/sh',
-                      '-c',
-                      'mongo --port ' + str(self.framework.model.config['advertised-port']) + ' --eval "rs.status()" | grep -vq "REMOVED"',
-                      ]},
-                      'initialDelaySeconds': 45,
-                      'timeoutSeconds': 5,
-                  }
-                }
+        self._mongo_builder = MongoBuilder(
+            self._framework_wrapper.app_name,
+            self._framework_wrapper.config,
+            self._resources,
+            self._framework_wrapper.goal_state_units
+        )
 
-    def _make_sidecar_spec(self):
-        pod_labels = "juju-app={}".format(self.framework.model.config['advertised-hostname'])
-        return {
-                'name': 'mongodb-sidecar-k8s',
-                'imageDetails': {
-                    'imagePath': self.sidecar_image.image_path,
-                    'username': self.sidecar_image.username,
-                    'password': self.sidecar_image.password,
-                },
-                'config': {
-                     'KUBERNETES_MONGO_SERVICE_NAME': self.framework.model.config['service-name'],
-                     'KUBE_NAMESPACE': self.framework.model.config['namespace'],
-                     'MONGO_SIDECAR_POD_LABELS': pod_labels,
-                     'KUBERNETES_CLUSTER_DOMAIN': self.framework.model.config['cluster-domain'],
-                },               
-               }
+        if self._framework_wrapper.config['enable-sidecar']:
+            self._resources['mongodb-sidecar-image'] = OCIImageResource('mongodb-sidecar-image')
 
+        self._pod = K8sPod(self._framework_wrapper.app_name)
 
-    def make_pod_spec(self, sidecar = False):
-        """Make pod specification for Kubernetes
+        delegators = [
+            (self.on.start, self.on_config_changed_delegator),
+            (self.on.upgrade_charm, self.on_config_changed_delegator),
+            (self.on.config_changed, self.on_config_changed_delegator),
+            (self.on.update_status, self.on_update_status_delegator),
+            (self.on.mongo_relation_joined, self.on_relation_changed_delegator),
+            (self.on.mongo_relation_changed, self.on_relation_changed_delegator),
+            (self.on.mongo_relation_departed, self.on_relation_changed_delegator),
+            (self.on.mongo_relation_broken, self.on_relation_changed_delegator),         
+        ]
+        for delegator in delegators:
+            self.framework.observe(delegator[0], delegator[1])
 
-        Returns:
-            pod_spec: Pod specification for Kubernetes
-        """
-        spec = {
-            'containers': [ self._make_container_spec() ] 
-            }
-        if sidecar:
-            spec['containers'].append( self._make_sidecar_spec() )
-        return spec
+    def on_config_changed_delegator(self, event):
+        logger.info('on_config_changed_delegator({})'.format(event))
+        return ConfigChangeObserver(
+                self._framework_wrapper, 
+                self._resources, 
+                self._pod,
+                self._mongo_builder).handle(event)
 
-    def on_start(self, event):
-        unit = self.framework.model.unit
-        has_sidecar = self.framework.model.config['enable-sidecar']
-        resources = self.framework.model.resources
-        if not self.mongodb_image.fetch(resources):
-            unit.status = BlockedStatus('Missing or invalid image resource')
-            return
-        if has_sidecar and not self.sidecar_image.fetch(resources):
-            unit.status = BlockedStatus('Missing or invalid image resource')
-            return
-        if not self.framework.model.unit.is_leader():
-            unit.status = WaitingStatus('Not leader')
-            return
-        spec = self.make_pod_spec( sidecar = has_sidecar)
-        unit.status = MaintenanceStatus('Configuring container')
-        self.framework.model.pod.set_spec(spec)
-        self.state.is_started = True
-        unit.status = ActiveStatus()
+    def on_relation_changed_delegator(self, event):
+        logger.info('on_relation_changed_delegator({})'.format(event))
+        return RelationObserver(
+                self._framework_wrapper, 
+                self._resources, 
+                self._pod,
+                self._mongo_builder).handle(event)
 
-    def on_update_status(self, event):
-        unit = self.framework.model.unit
-        unit.status = ActiveStatus()
-
-    def goal_state(self):
-        """Juju goal state values"""
-        cmd = ['goal-state', '--format=json']
-        return json.loads(subprocess.check_output(cmd).decode('UTF-8'))
-
-    def on_mongo_relation_joined(self, event):
-        mongo_uri = "mongodb://"
-        for i, unit in enumerate(self.goal_state()['units']):
-            pod_base_name = unit.split('/')[0]
-            service_name = self.framework.model.config['service-name']
-            pod_name = "{}-{}".format(pod_base_name, i)
-            if i:
-                mongo_uri += ","
-            mongo_uri += "{}.{}:{}".format(pod_name, service_name,
-                                           self.framework.model.config['advertised-port'])
-        if self.framework.model.config['enable-sidecar']:
-            mongo_uri += "/?replicaSet={}".format(self.framework.model.config['replica-set'])
-        print("MongoDB URI: " + mongo_uri)
-        relation = event.relation
-        relation.data[self.framework.model.unit]['connection_string']=mongo_uri
+    def on_update_status_delegator(self, event):
+        logger.info('on_update_status_delegator({})'.format(event))
+        return StatusObserver(
+                self._framework_wrapper, 
+                self._resources, 
+                self._pod,
+                self._mongo_builder).handle(event)
 
 if __name__ == "__main__":
     main(MongoDbCharm)
---
+++ src/observers.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+import re
+import sys
+sys.path.append('lib')
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    WaitingStatus,
+    MaintenanceStatus,
+)
+import logging
+
+logger = logging.getLogger()
+
+class BaseObserver:
+
+    def __init__(self, framework, resources, pod, builder):
+        self._framework = framework
+        self._resources = resources
+        self._pod = pod
+        self._builder = builder
+    
+    @abstractmethod
+    def handle(self, event):
+        pass    
+
+class ConfigChangeObserver(BaseObserver):
+
+    def handle(self, event):
+        for resource in self._resources.keys():
+            if not self._resources[resource].fetch(self._framework.resources):
+                self._framework.unit_status_set(
+                    BlockedStatus('Missing or invalid image resource: {}'.format(resource)))
+                return
+        if not self._framework.unit_is_leader:
+            self._framework.unit_status_set(WaitingStatus('Not the leader'))
+            return
+
+        spec = self._builder.build_spec()
+        self._framework.unit_status_set(MaintenanceStatus('Configuring container'))
+        self._framework.pod_spec_set(spec)
+        if self._pod.is_ready:
+            self._framework.unit_status_set(ActiveStatus('ready'))
+            return
+        self._framework.unit_status_set(MaintenanceStatus('Pod is not ready'))      
+
+class RelationObserver(BaseObserver):
+
+    def handle(self, event):
+        handler_name = "handle_" + \
+                re.sub(r'(?<!^)(?=[A-Z])', '_',
+                       event.__class__.__name__).lower()
+        try:
+            handler = self.__getattribute__(handler_name)
+        except AttributeError as e:
+            print("Handler {} undefined, ignoring".format(handler_name))
+            return
+        handler(event)
+
+    def handle_relation_joined_event(self, event):
+        data = self._builder.build_relation_data()
+        self._framework.relation_data_set(event.relation, data)    
+
+class StatusObserver(BaseObserver):
+
+    def handle(self, event):
+        if self._pod.is_ready:
+            self._framework.unit_status_set(ActiveStatus())
+            return
+        self._framework.unit_status_set(BlockedStatus('Pod is not ready'))
+        
